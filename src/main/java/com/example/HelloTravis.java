abc723d6873b066c53892a33399572da847e72f1
takahitosuzukiniandc <35447064+takahitosuzukiniandc@users.noreply.github.com>
2018-01-19 10:07:52 +0900

    Update HelloTravis.java

--- src/main/java/com/example/HelloTravis.java
+++ src/main/java/com/example/HelloTravis.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloTravis {
     public static void main(String[] args) {
-        System.out.println("Hello Travis!");
+        System.out.println("Hello Travisaaaa!");
     }
 }
